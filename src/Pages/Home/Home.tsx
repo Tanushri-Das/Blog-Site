4c87e7fc46b84a6f966cef0be0370d3881b42fad
tanu <dastanushri402@gmail.com>
2024-04-22 10:26:10 +0600

    add some homepage content

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="my-20">
+      <h1 className="text-black text-center text-4xl mb-8 font-bold">
+        Welcome to my Blog Website
+      </h1>
+      <p className="text-black text-center text-xl mb-6 font-medium">
+        Can you add blog ? Please click this button
+      </p>
+      <div className="flex justify-center items-center">
+        <Link to="/create-blogs">
+          <button className="login-btn text-lg font-semibold text-white px-8 py-3">
+            Create Blogs
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
